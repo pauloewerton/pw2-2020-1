0ca68a38c70c1c56f9077e754f0164b2fee9600c
Paulo Ewerton <pauloewerton@icloud.com>
2020-11-06 17:26:04 -0300

    Código da Aula 06 - Bootstrap e Thymeleaf

--- aula05/src/main/java/br/edu/ifpb/aula05/controller/AgendaController.java
+++ aula05/src/main/java/br/edu/ifpb/aula05/controller/AgendaController.java
@@ -4,6 +4,7 @@ import br.edu.ifpb.aula05.model.Agenda;
 import br.edu.ifpb.aula05.service.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +27,15 @@ public class AgendaController {
         ModelAndView mv = new ModelAndView("agenda");
         List<Agenda> agendaList = agendaService.findAll();
         mv.addObject("agenda", agendaList);
+
+        return mv;
+    }
+
+    @RequestMapping(path = {"/agenda/{id}"}, method = {RequestMethod.GET})
+    public ModelAndView getContato(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("contato");
+        Agenda agenda = agendaService.findById(id);
+        mv.addObject("contato", agenda);
 
         return mv;
     }
