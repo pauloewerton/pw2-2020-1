ebb4d3ea38d626b92edb1083d048abb634365044
Paulo Ewerton <pauloewerton@icloud.com>
2020-11-20 17:23:31 -0300

    Aula 08

---
+++ aula05/src/main/java/br/edu/ifpb/aula05/configuration/SecurityConfig.java
@@ -0,0 +1,50 @@
+package br.edu.ifpb.aula05.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_LIST = {
+            "/",
+            "/agenda",
+            "/agenda/{id}"
+    };
+
+    private static final String ADMIN_ROLE = "ADMIN";
+    private static final String USER_ROLE = "USER";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("pauloewerton")
+                .password("{noop}paulo123")
+                .roles(ADMIN_ROLE);
+        auth.inMemoryAuthentication()
+                .withUser("paulo")
+                .password("{noop}paulo123")
+                .roles(USER_ROLE);
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers(AUTH_LIST).permitAll()
+                .antMatchers("/novocontato").hasRole(ADMIN_ROLE)
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/boostrap/**", "/style/**");
+    }
+}
